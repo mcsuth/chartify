911f1b6345b13282e9176a4e5544abb5e8e4db43
WizardNinja89 <winjacan@gmail.com>
2013-11-11 14:23:18 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbAuth::Application.config.secret_token = 'dc09a9dde36bb8b1d4677c307ffa2e14e2e809effd3e85a220438edd536cceb8d3a24c08c52dd76039187e875b84efca7656485419222d4f50f4787d585f6d67'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+FbAuth::Application.routes.draw do
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: 'sessions#create'
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
+  root to: "main#index"
+end
