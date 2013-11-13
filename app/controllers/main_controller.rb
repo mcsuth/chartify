4a115aaa26d0ae59386b63517ab4bba3ce367eed
WizardNinja89 <winjacan@gmail.com>
2013-11-12 16:44:00 -0800

    user now gets asked for the permissions we need to get their data

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
 	def index
+		unless current_user
+			redirect_to 'login'
+		end
+	end
+
+	def login
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ FbAuth::Application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: 'sessions#create'
   match 'signout', to: 'sessions#destroy', as: 'signout'
+  match 'login', to: 'main#login'
 
   root to: "main#index"
 end
