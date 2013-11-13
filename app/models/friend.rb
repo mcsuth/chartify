df67b20d69e9ce62e15e36a68cece91fd2823eb5
WizardNinja89 <winjacan@gmail.com>
2013-11-13 14:04:20 -0800

    friends get added to database on login

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,4 +2,6 @@ class Friend < ActiveRecord::Base
   attr_accessible :profile_picture, :username, :facebook_id, :user_id
 
   belongs_to :user
+
+  validates :facebook_id, uniqueness: { :scope => :user_id }
 end
