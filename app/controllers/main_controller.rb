fa718a0ebcc64bd04ad4c1771abb6b73481ea389
WizardNinja89 <winjacan@gmail.com>
2013-11-12 19:22:00 -0800

    added koala gem, added route to get users posts messages and likes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,4 +10,22 @@ class MainController < ApplicationController
 
   end
 
+  def post_messages
+  	posts = current_user.facebook.get_connections("me", "posts")
+  	messages = ""
+  	likes = []
+  	posts.each do |post|
+  		if post['message']
+  			messages += post['message']
+  		end
+  		if post['likes']
+  			likes += post['likes']['data']
+  		end
+  	end
+  	@data = {messages: messages, likes: likes}
+
+
+  	render json: @data
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ FbAuth::Application.routes.draw do
   match 'auth/failure', to: 'sessions#create'
   match 'signout', to: 'sessions#destroy', as: 'signout'
   match 'login', to: 'main#login'
+  match 'post_messages', to: 'main#post_messages'
 
   root to: "main#index"
 end
