c851e65659a7265a9bd42db647c0ec36f6fe35b3
WizardNinja89 <winjacan@gmail.com>
2013-11-16 16:44:37 -0800

    added sidekiq to gemfile

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 gem 'rails', '3.2.14'
 gem 'unicorn'
+gem 'sidekiq'
 
 group :develepmont, :test do
   gem 'rspec-rails'
--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -3,21 +3,21 @@ class HardWorker
 
 	def perform(posts)
 
-		posts.each do |post|
-			if post['message']
-				puts post['message']
-			end
-		end
+		# posts.each do |post|
+		# 	if post['message']
+		# 		puts post['message']
+		# 	end
+		# end
 
-  	next_page = posts.next_page
-  	while !next_page.empty?
-  		posts = next_page
-  		posts.each do |post|
-  			if post['message']
-  				puts post['message']
-  			end
-  		end
-  		next_page = next_page.next_page
-  	end
+  # 	next_page = posts.next_page
+  # 	while !next_page.empty?
+  # 		posts = next_page
+  # 		posts.each do |post|
+  # 			if post['message']
+  # 				puts post['message']
+  # 			end
+  # 		end
+  # 		next_page = next_page.next_page
+  # 	end
 	end
 end
